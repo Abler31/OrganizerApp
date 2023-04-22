8a42b9b96391cd99b013f465739961b4aa100206
Abler31 <58187511+Abler31@users.noreply.github.com>
2023-04-22 16:01:59 +0300

    Navigation between bottom navigation fragments implemented

--- app/src/main/java/com/my/org/presentation/MainFragment.kt
+++ app/src/main/java/com/my/org/presentation/MainFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.my.org.R
 
 class MainFragment : Fragment() {
@@ -13,8 +16,12 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+        val bottomNavigationView = view.findViewById<BottomNavigationView>(R.id.mainBottomNavigationView)
+        val navController = (childFragmentManager.findFragmentById(R.id.mainContainerView) as NavHostFragment)
+            .navController
+        bottomNavigationView.setupWithNavController(navController)
+        return view
     }
 
 }
